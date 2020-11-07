16d29c6af8fe803dd467272c1aa69aa4e95da5f6
Nuwan <nuwan.wickramasingha@cerexio.com>
2020-11-07 23:59:15 +0530

    5. Styling React Components & Elements:7. Introducing Styled Components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import Radium,{StyleRoot}  from 'radium';
+// import Radium,{StyleRoot}  from 'radium';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -86,7 +86,7 @@ class App extends Component {
       classes.push('bold');
     }
     return (
-        <StyleRoot>
+        // <StyleRoot>
           <div className="App">
             <h1>Hi, I'm a React App</h1>
             <p className={classes.join(' ')}>This is really working!</p>
@@ -95,11 +95,12 @@ class App extends Component {
                 onClick={this.togglePersonsHandler}>Switch Name</button>
             {persons}
           </div>
-        </StyleRoot>
+        // </StyleRoot>
 
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
-export default Radium(App);
+// export default Radium(App);
+export default App;

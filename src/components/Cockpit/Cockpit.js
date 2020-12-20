e57f18d7bd55565d8216b6e2cff8e054b6466dc2
Nuwan <nuwan.wickramasingha@cerexio.com>
2020-12-20 18:16:45 +0530

    7. Diving Deeper into Components & React Internals:13. Cleanup Work with useEffect() - Example

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -5,10 +5,11 @@ const  cockpit = (props) => {
     useEffect(() => {
         console.log('[Cockpit.js] useEffect');
         // http req..
-        setTimeout(()=>{
+        const timer = setTimeout(()=>{
             alert('Saved data to the cloud!')
         },1000);
         return () => {
+            clearTimeout(timer);
             console.log('[Cockpit.js] cleanup work in useEffect');
         }
     },[]);
